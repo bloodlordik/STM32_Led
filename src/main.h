c057897da91969cecc6a238375201a37b002bf09
bloodlordik <bloodlordik@yandex.ru>
2024-05-22 10:25:50 +0700

    extract functions

--- src/main.h
+++ src/main.h
@@ -23,3 +23,46 @@
 #define LED_PIN                                GPIO_PIN_5
 #define LED_GPIO_PORT                          GPIOA
 #define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOA_CLK_ENABLE()
+
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
+}
+
+void NMI_Handler(void)
+{
+}
+
+void HardFault_Handler(void)
+{
+  while (1) {}
+}
+
+
+void MemManage_Handler(void)
+{
+  while (1) {}
+}
+
+void BusFault_Handler(void)
+{
+  while (1) {}
+}
+
+void UsageFault_Handler(void)
+{
+  while (1) {}
+}
+
+void SVC_Handler(void)
+{
+}
+
+
+void DebugMon_Handler(void)
+{
+}
+
+void PendSV_Handler(void)
+{
+}
